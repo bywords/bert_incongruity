7222f2b7351fad0b64d402114d254d6bf9dbb3cc
bywords <bywords.kor@gmail.com>
2019-11-19 12:38:52 +0300

    for debugging

--- bert_pool.py
+++ bert_pool.py
@@ -21,22 +21,17 @@ class BertPoolForIncongruity(nn.Module):
         headline_outputs = self.bert(headline_input_ids, token_type_ids=headline_token_type_ids)[0]  # last hidden states
         bodytext_outputs = self.bert(bodytext_input_ids, token_type_ids=bodytext_token_type_ids)[0]  # last hidden states
 
-        print(headline_outputs.size())
-        print(headline_pool_masks.size())
-        print(headline_pool_masks.size())
-        print(headline_lens.size())
+        print(headline_outputs.size())  # 64, 512, 768
+        print(headline_pool_masks.size())  # 64, 512, 1
+        print(headline_lens.size())  # 64, 1, 1
 
         # print(headline_pool_masks[0])
         # print(headline_token_type_ids[0])
 
-        temp = torch.matmul(torch.transpose(headline_outputs, 1, 2), headline_pool_masks).squeeze()
-        print(temp.size())
-        # headline_temp = headline_lens.expand(headline_lens.size(0), temp.size(1))
-        # print(headline_temp.size())
-        # temp2 = torch.div(temp, headline_temp)
-        # print(temp2.size())
+        temp = torch.matmul(torch.transpose(headline_outputs, 1, 2), headline_pool_masks)
+        print(temp.size())  # (64, 768, 1)
         temp3 = torch.div(temp, headline_lens)
-        print(temp3.size())
+        print(temp3.size())  # 64, 768, 1
 
 
 
