f5570e67ff3124524f822dbf6b81b18df3de8f4b
bywords <bywords.kor@gmail.com>
2019-11-26 11:23:04 +0300

    filter data

---
+++ filter_data.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+import os
+import argparse
+import pandas as pd
+
+
+def filter_data(path):
+    df = pd.read_csv(path, sep="\t")
+
+    filter_func = lambda x: len(x.replace('<EOS>', '').replace('<EOP>', '').strip()) > 0
+    new_df = df.loc[df[1].apply(filter_func) & df[2].apply(filter_func)]
+    new_df.to_csv(path, index=False, header=False, sep="\t")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    ## Required parameters
+    parser.add_argument("--data_dir", default=None, type=str, required=True,
+                        help="data directory")
+
+    args = parser.parse_args()
+    filter_data(os.path.join(args.data_dir, "train.tsv"))
+    filter_data(os.path.join(args.data_dir, "dev.tsv"))
+    filter_data(os.path.join(args.data_dir, "test.tsv"))
