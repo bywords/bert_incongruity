01603ce9e638bc66e959e9eda3dde7320b4412ff
bywords <bywords.kor@gmail.com>
2019-12-05 11:30:57 +0300

    fix argument error

--- test_for_temporary.py
+++ test_for_temporary.py
@@ -34,8 +34,7 @@ def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # output setups
-    exp_id = "data-{}_model-pool_freeze-{}_seed-{}".format(args.data_dir, args.model,
-                                                         args.freeze, args.seed)
+    exp_id = "data-{}_model-pool_freeze-{}_seed-{}".format(args.data_dir, args.freeze, args.seed)
     exp_dir = os.path.join(args.output_dir, exp_id)
     if not os.path.exists(exp_dir):
         os.makedirs(exp_dir)
