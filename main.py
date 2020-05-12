10989817ccb50b62831d7a685c521f90d0342205
Kunwoo Park <bywords.kor@gmail.com>
2020-05-12 11:11:34 -0700

    fix errors

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def main(args):
         else:
             model.unfreeze_bert_encoder()
 
-        print(model.state_dict())
+        print([k for k, t in model.state_dict()])
         exit()
 
 
