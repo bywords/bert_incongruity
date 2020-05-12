2bc0863d45c099192ea34d29ced6dd1b73c4f3e4
Kunwoo Park <bywords.kor@gmail.com>
2020-05-12 11:10:08 -0700

    fix errors

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # output setups
-    exp_id = "data-{}_model-{}_freeze-{}_seed-{}".format(args.data_dir, args.model,
+    exp_id = "data-{}_model-{}_freeze-{}_seed-{}".format(args.data_dir, args.model_file,
                                                          args.freeze, args.seed)
     exp_dir = os.path.join(args.output_dir, exp_id)
     if not os.path.exists(exp_dir):
