f35a9349e113b9ba5f9af53ede2b9b3bf4f01374
Kunwoo Park <bywords.kor@gmail.com>
2020-05-25 14:41:03 -0700

    for debugging

--- main_nsp_freeze.py
+++ main_nsp_freeze.py
@@ -139,7 +139,7 @@ def main(args):
 
             preds = prediction_sm.detach().cpu().numpy()[:, 1].tolist()
 
-            print(preds.shape)
+            # print(preds.shape)
             y_preds.append(preds)
             print(y_preds)
             break
